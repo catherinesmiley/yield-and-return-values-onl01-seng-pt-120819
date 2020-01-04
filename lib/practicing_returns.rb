b287950fd43273b62910ddb6c72a85249f7b944b
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-04 20:48:36 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,11 +1,39 @@
+# def hello(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end
+
+# hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def hello(array)
-  i = 0
+  i = 0 
   collection = []
   while i < array.length
     collection << yield(array[i])
-    i += 1
+    i += 1 
   end
   collection
 end
-
 hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
